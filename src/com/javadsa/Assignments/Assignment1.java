e859314b6d9b7349a1b9759b5edfb57736851a71
meryjoshna <meryjoshna2000@gmail.com>
2024-09-22 17:34:27 +0530

    assignment started
    first question even or odd completed

--- src/com/javadsa/Assignments/Assignment1.java
+++ src/com/javadsa/Assignments/Assignment1.java
@@ -1,4 +1,29 @@
 package com.javadsa.Assignments;
-
+import java.util.Scanner;
+//Write a program to print whether a number is even or odd, also take input from the user.
+//Take name as input and print a greeting message for that particular name.
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+//Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+//Take 2 numbers as input and print the largest number.
+//Input currency in rupees and output in USD.
+//To calculate Fibonacci Series up to n numbers.
+//To find out whether the given String is Palindrome or not.
+//To find Armstrong Number between two given number.
 public class Assignment1 {
+
+    public static void main(String[] args) {
+        evenOrOdd();
+    }
+    static void evenOrOdd(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n =sc.nextInt();
+        if(n % 2 ==0){
+            System.out.println(n+" is even number");
+        }
+        else{
+            System.out.println(n+" is odd number");
+        }
+    }
+
 }
